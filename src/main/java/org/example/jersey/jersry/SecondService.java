7fd538a32c45a3819c983b273729780ede031ce8
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-10-12 00:43:58 +0500

    Added Jersey

---
+++ src/main/java/org/example/jersey/jersry/SecondService.java
@@ -0,0 +1,12 @@
+package org.example.jersey.jersry;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecondService {
+
+    public String getString(String getString) {
+        return getString;
+    }
+}
---
+++ src/test/java/org/example/jersey/JerseyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jersey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JerseyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
